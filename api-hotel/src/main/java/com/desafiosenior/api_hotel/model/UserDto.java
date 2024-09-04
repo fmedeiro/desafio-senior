3d246fc17acdd89603ac91defe0b1630cf73b53b
fmedeiro <fmedeiros13@yahoo.com.br>
2024-09-04 16:06:41 -0300

    Changed indentation and changed label.

--- api-hotel/src/main/java/com/desafiosenior/api_hotel/model/UserDto.java
+++ api-hotel/src/main/java/com/desafiosenior/api_hotel/model/UserDto.java
@@ -6,36 +6,36 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserDto(
-		@NotBlank
-		@Pattern(regexp = "\\d{9,14}", message = "O campo deve conter entre 9 e 14 dígitos numéricos.")
-		String document,
-		
-		@NotBlank @Email @Size(min = 4, max = 50, message = "Este campo tem que ter de 4 a 50 caracteres.") 
-		String email,
-		
-		@NotBlank @Size(min = 4, max = 12, message = "Este campo tem que ter de 4 a 12 caracteres.")
-		String login, 
-		
-		@NotBlank @Size(min = 4, max = 60, message = "Este campo tem que ter de 4 a 60 caracteres.") 
-		String name,
-		
-		@NotBlank @Size(min = 4, max = 8, message = "Este campo tem que ter de 4 a 8 caracteres.") 
-		String password,
-		
-		@NotBlank
-		@Pattern(regexp = "\\d{8,9}", message = "O campo deve conter entre 8 e 9 dígitos numéricos.")
-		String phone,
-		
-		@NotBlank
-		@Pattern(regexp = "^\\d{2}$", message = "O campo deve conter 2 dígitos numéricos.")
-		String phoneDdd,
-		
-		@NotBlank
-		@Pattern(regexp = "^\\d{2}$", message = "O campo deve conter 2 dígitos numéricos.")
-		String phoneDdi,
-		
-		@NotBlank
-		@Pattern(regexp = "^[AaGgUu]$", message = "O campo deve conter apenas uma das letras: A, G ou U, maiúsculas ou minúsculas.") 
-		String role
-		) {
+	@NotBlank
+	@Pattern(regexp = "\\d{9,14}", message = "O campo deve conter entre 9 e 14 dígitos numéricos.")
+	String document,
+	
+	@NotBlank @Email @Size(min = 4, max = 50, message = "Este campo tem que ter de 4 a 50 caracteres.") 
+	String email,
+	
+	@NotBlank @Size(min = 4, max = 12, message = "Este campo tem que ter de 4 a 12 caracteres.")
+	String login, 
+	
+	@NotBlank @Size(min = 4, max = 60, message = "Este campo tem que ter de 4 a 60 caracteres.") 
+	String name,
+	
+	@NotBlank @Size(min = 4, max = 8, message = "Este campo tem que ter de 4 a 8 caracteres.") 
+	String password,
+	
+	@NotBlank
+	@Pattern(regexp = "\\d{8,9}", message = "O campo deve conter entre 8 e 9 dígitos numéricos.")
+	String phone,
+	
+	@NotBlank
+	@Pattern(regexp = "^\\d{2}$", message = "O campo deve conter 2 dígitos numéricos.")
+	String phoneDdd,
+	
+	@NotBlank
+	@Pattern(regexp = "^\\d{2}$", message = "O campo deve conter 2 dígitos numéricos.")
+	String phoneDdi,
+	
+	@NotBlank
+	@Pattern(regexp = "^[AaGgUu]$", message = "O campo deve conter apenas uma das letras: A (ADMIN), G (GUEST) ou U (USER_ATTENDANT), maiúsculas ou minúsculas.") 
+	String role
+	) {
 }
