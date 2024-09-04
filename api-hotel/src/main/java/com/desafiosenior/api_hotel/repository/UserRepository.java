94e99f4afb937fc59a133f3c1a7b39f9d82290ee
fmedeiro <fmedeiros13@yahoo.com.br>
2024-09-04 16:09:24 -0300

    Added new find users respecting the functional specification.

--- api-hotel/src/main/java/com/desafiosenior/api_hotel/repository/UserRepository.java
+++ api-hotel/src/main/java/com/desafiosenior/api_hotel/repository/UserRepository.java
@@ -5,12 +5,21 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import com.desafiosenior.api_hotel.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 	Optional<UserDetails> findByLogin(String login);
+
 	Optional<User> findByUserId(UUID userId);
+
+	Optional<User> findByPhoneDdiAndPhoneDddAndPhone(String phoneDdi, String phoneDdd, String phone);
+	
+	Optional<User> findByName(String name);
+	
 	Optional<User> findByEmail(String email);
+
 	Optional<User> findByDocument(String document);
 }
