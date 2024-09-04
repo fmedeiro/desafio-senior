588b0a6415feefaf9838ad1c8d5beed2adcf7fc8
fmedeiro <fmedeiros13@yahoo.com.br>
2024-09-04 16:07:23 -0300

    Changed indentation.

--- api-hotel/src/main/java/com/desafiosenior/api_hotel/model/UserRole.java
+++ api-hotel/src/main/java/com/desafiosenior/api_hotel/model/UserRole.java
@@ -3,14 +3,12 @@ package com.desafiosenior.api_hotel.model;
 import lombok.Getter;
 
 public enum UserRole {
-	ADMIN("A"),
-	GUEST("G"),
-	USER_ATTENDANT("U");
+	ADMIN("A"), GUEST("G"), USER_ATTENDANT("U");
 
 	@Getter
-    private String role;
+	private String role;
 
-	UserRole(String role){
-        this.role = role;
-    }
+	UserRole(String role) {
+		this.role = role;
+	}
 }
