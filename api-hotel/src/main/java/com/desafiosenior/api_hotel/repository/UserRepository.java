6da5d92f00d0a1e6362de7648cd64a6a8b8af5b5
fmedeiro <fmedeiros13@yahoo.com.br>
2024-09-04 21:17:35 -0300

    Removed some unnecessary findBy prefix methods.

--- api-hotel/src/main/java/com/desafiosenior/api_hotel/repository/UserRepository.java
+++ api-hotel/src/main/java/com/desafiosenior/api_hotel/repository/UserRepository.java
@@ -14,18 +14,10 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 	Optional<UserDetails> findByLogin(String login);
 
 	Optional<User> findByUserId(UUID userId);
-
-	Optional<User> findByPhoneDdiAndPhoneDddAndPhone(String phoneDdi, String phoneDdd, String phone);
 	
 	Optional<User> findByPhoneDdiAndPhoneDddAndPhoneAndRole(String phoneDdi, String phoneDdd, String phone, String role);
-	
-	Optional<User> findByName(String name);
-	
+		
 	Optional<User> findByNameAndRole(String name, String role);
-	
-	Optional<User> findByEmail(String email);
-
-	Optional<User> findByDocument(String document);
 	
 	Optional<User> findByDocumentAndRole(String document, String role);
 }
