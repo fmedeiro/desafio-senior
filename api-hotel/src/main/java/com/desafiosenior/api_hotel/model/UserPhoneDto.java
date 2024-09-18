5d54ee3e8052b124e8b952d74274e4cd31baf802
fmedeiro <fmedeiros13@yahoo.com.br>
2024-09-18 09:03:43 -0300

    Increased number of characters to respect internationalization rules.

--- api-hotel/src/main/java/com/desafiosenior/api_hotel/model/UserPhoneDto.java
+++ api-hotel/src/main/java/com/desafiosenior/api_hotel/model/UserPhoneDto.java
@@ -5,15 +5,15 @@ import jakarta.validation.constraints.Pattern;
 
 public record UserPhoneDto(	
 	@NotBlank
-	@Pattern(regexp = "^\\d{2}$", message = "O campo deve conter 2 dígitos numéricos.")
+	@Pattern(regexp = "^\\d{1,2}$", message = "O campo deve conter entre 1 e 2 dígitos numéricos.")
 	String phoneDdi,
 	
 	@NotBlank
-	@Pattern(regexp = "^\\d{2}$", message = "O campo deve conter 2 dígitos numéricos.")
+	@Pattern(regexp = "^\\d{1,3}$", message = "O campo deve conter entre 1 e 3 dígitos numéricos.")
 	String phoneDdd,
 	
 	@NotBlank
-	@Pattern(regexp = "\\d{8,9}", message = "O campo deve conter entre 8 e 9 dígitos numéricos.")
+	@Pattern(regexp = "\\d{8,10}", message = "O campo deve conter entre 8 e 10 dígitos numéricos.")
 	String phone
 	) {
 }
