4caacece98232bcc226211af097d7ae8e2c50a66
fmedeiro <fmedeiros13@yahoo.com.br>
2024-09-23 20:26:13 -0300

    Code refactored to use the labels from the messages.properties.

--- api-hotel/src/main/java/com/desafiosenior/api_hotel/model/UserDto.java
+++ api-hotel/src/main/java/com/desafiosenior/api_hotel/model/UserDto.java
@@ -11,35 +11,35 @@ public record UserDto(
 	List<Booking> bookings,
 		
 	@NotBlank
-	@Pattern(regexp = "\\d{9,14}", message = "O campo deve conter entre 9 e 14 dígitos numéricos.")
+	@Pattern(regexp = "\\d{9,14}", message = "{label.document.valid.format.size}")
 	String document,
 	
-	@NotBlank @Email @Size(min = 4, max = 50, message = "Este campo tem que ter de 4 a 50 caracteres.") 
+	@NotBlank @Email @Size(min = 4, max = 50, message = "{label.email.valid.format.size}") 
 	String email,
 	
-	@NotBlank @Size(min = 4, max = 12, message = "Este campo tem que ter de 4 a 12 caracteres.")
+	@NotBlank @Size(min = 4, max = 12, message = "{label.login.valid.format.size}")
 	String login, 
 	
-	@NotBlank @Size(min = 4, max = 60, message = "Este campo tem que ter de 4 a 60 caracteres.") 
+	@NotBlank @Size(min = 4, max = 60, message = "{label.name.valid.format.size}") 
 	String name,
 	
-	@NotBlank @Size(min = 4, max = 8, message = "Este campo tem que ter de 4 a 8 caracteres.") 
+	@NotBlank @Size(min = 4, max = 8, message = "{label.password.valid.format.size}") 
 	String password,
 	
 	@NotBlank
-	@Pattern(regexp = "\\d{8,10}", message = "O campo deve conter entre 8 e 10 dígitos numéricos.")
+	@Pattern(regexp = "\\d{8,10}", message = "{label.phone.valid.format.size}")
 	String phone,
 	
 	@NotBlank
-	@Pattern(regexp = "^\\d{1,3}$", message = "O campo deve conter entre 1 e 3 dígitos numéricos.")
+	@Pattern(regexp = "^\\d{1,3}$", message = "{label.phoneDdd.valid.format.size}")
 	String phoneDdd,
 	
 	@NotBlank
-	@Pattern(regexp = "^\\d{1,2}$", message = "O campo deve conter entre 1 e 2 dígitos numéricos.")
+	@Pattern(regexp = "^\\d{1,2}$", message = "{label.phoneDdi.valid.format.size}")
 	String phoneDdi,
 	
 	@NotBlank
-	@Pattern(regexp = "^[AaGgUu]$", message = "O campo deve conter apenas uma das letras: A (ADMIN), G (GUEST) ou U (USER_ATTENDANT), maiúsculas ou minúsculas.") 
+	@Pattern(regexp = "^[AaGgUu]$", message = "{label.role.valid.format.size}") 
 	String role
 	) {
 }
