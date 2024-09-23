24464d58147043ab21aa8a04c9a4c9caaf3575ce
fmedeiro <fmedeiros13@yahoo.com.br>
2024-09-23 20:15:32 -0300

    Code refactored to add to MessageSource.

--- api-hotel/src/test/java/com/desafiosenior/api_hotel/util/Utils.java
+++ api-hotel/src/test/java/com/desafiosenior/api_hotel/util/Utils.java
@@ -1,7 +1,25 @@
 package com.desafiosenior.api_hotel.util;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import jakarta.validation.ConstraintViolation;
+
+@Component
 public class Utils {
-	public static <T> boolean assertEqualsWithOr(T actual, @SuppressWarnings("unchecked") T... expectedValues) {
+	
+	private final MessageSource messageSource;
+	
+	public Utils(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+	public <T> boolean assertEqualsWithOr(T actual, @SuppressWarnings("unchecked") T... expectedValues) {
 		if (actual == null) return false;
 		for (T expected : expectedValues) {
 			if (actual.equals(expected)) {
@@ -9,5 +27,14 @@ public class Utils {
 			}
 		}
 		return false;
+	}
+	
+	public List<String> getMessagesErrors(Set<? extends ConstraintViolation<?>> violations) {
+	    return violations.stream()
+	        .map(violation -> messageSource.getMessage(
+	            violation.getMessage().replaceAll("[\\{\\}]", ""), 
+	            null, 
+	            LocaleContextHolder.getLocale()))
+	        .collect(Collectors.toList());
 	}
 }
